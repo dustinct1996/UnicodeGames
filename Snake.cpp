2df506da0ceb52e3a9c1016db7660fa67c5d5d25
dustinct1996 <50458389+dustinct1996@users.noreply.github.com>
2020-05-12 01:29:51 -0400

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -11,40 +11,16 @@
 using namespace std;
 using std::fstream;
 
-void sortLeaderBoard(const char *fileName)
-{
-	vector<int> scores;
-	fstream leaderBoard;
-
-	leaderBoard.open(fileName);
-
-	string num;
-
-	while (getline(leaderBoard, num))
-		scores.push_back(stoi(num));
-
-	leaderBoard.close();
-
-	leaderBoard.open("snakeLeaderBoard.txt");
-
-	sort(scores.begin(), scores.end(), greater<int>());
-
-	for (int i = 0; i < scores.size(); i++)
-		leaderBoard << scores.at(i) << "\n";
-
-	leaderBoard.close();
-}
-
 int Snake()
 {
 	vector<vector<int>> snake = { { 40, 15 } , { 39, 15 } , { 38, 15 } , { 37, 15 } };
 	fstream leaderBoard;
-	string num = "1";
+	string oldHighScore = "0";
 	int bKey[4];
 	int quit = 0;
 	int score = 0;
-	int newGame = 0;
 	int numEggs = 0;
+	int newGame = 0;
 	int gameOver = 0;
 	int direction = 2;
 	int screenWidth = 80;
@@ -54,7 +30,7 @@ int Snake()
 	int screenArea = screenWidth * screenHeight;
 	int eggPosX = rand() % screenWidth;
 	int eggPosY = (rand() % (screenHeight - 3)) + 3;
-	wchar_t *screen = new wchar_t[screenArea];
+	wchar_t* screen = new wchar_t[screenArea];
 
 	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
@@ -88,49 +64,51 @@ int Snake()
 
 		switch (direction)
 		{
-			case 0:
-				snake.insert(snake.begin(), { --curHeadPosX, curHeadPosY });
-				break;
-			case 1:
-				snake.insert(snake.begin(), { curHeadPosX, --curHeadPosY });
-				break;
-			case 2:
-				snake.insert(snake.begin(), { ++curHeadPosX, curHeadPosY });
-				break;
-			case 3:
-				snake.insert(snake.begin(), { curHeadPosX, ++curHeadPosY });
-				break;
+		case 0:
+			snake.insert(snake.begin(), { --curHeadPosX, curHeadPosY });
+			break;
+		case 1:
+			snake.insert(snake.begin(), { curHeadPosX, --curHeadPosY });
+			break;
+		case 2:
+			snake.insert(snake.begin(), { ++curHeadPosX, curHeadPosY });
+			break;
+		case 3:
+			snake.insert(snake.begin(), { curHeadPosX, ++curHeadPosY });
+			break;
 		}
 
 		if ((snake.at(0).at(0) < 0 || snake.at(0).at(0) >= screenWidth || snake.at(0).at(1) < 3 || snake.at(0).at(1) >= screenHeight) || (screen[snake.at(0).at(1) * screenWidth + snake.at(0).at(0)] == L"o"[0]))
 		{
 			snake.erase(snake.begin());
-
-			leaderBoard.open("snakeLeaderBoard.txt");
-
-			if (leaderBoard.peek() != std::ifstream::traits_type::eof())
-			{
-				getline(leaderBoard, num);
-			}
-
-			leaderBoard.close();
-
-			leaderBoard.open("snakeLeaderBoard.txt", fstream::app);
-
-			leaderBoard << score << "\n";
-
-			leaderBoard.close();
-
-			sortLeaderBoard("snakeLeaderBoard.txt");
-
 			gameOver = true;
+
+			leaderBoard.open("snakeHighScore.txt");
+
+			if (leaderBoard.peek() != ifstream::traits_type::eof())
+				getline(leaderBoard, oldHighScore);
+
+			leaderBoard.close();
+
+			if (score > stoi(oldHighScore))
+			{
+				wsprintf(&screen[10 * screenWidth + 33], L"NEW HIGH SCORE");
+
+				leaderBoard.open("snakeHighScore.txt", fstream::out);
+
+				leaderBoard << score << "\n";
+
+				leaderBoard.close();
+			}
+			else
+				wsprintf(&screen[10 * screenWidth + 28], L"CURRENT HIGH SCORE: %d", stoi(oldHighScore));
 		}
 		else
 		{
 			screen[snake.at(snake.size() - 1).at(1) * screenWidth + snake.at(snake.size() - 1).at(0)] = L" "[0];
 			snake.pop_back();
 		}
-		
+
 		if (screen[snake.at(0).at(1) * screenWidth + snake.at(0).at(0)] == L"0"[0])
 		{
 			numEggs++;
@@ -142,7 +120,7 @@ int Snake()
 				eggPosY = (rand() % (screenHeight - 3)) + 3;
 			} while (screen[eggPosY * screenWidth + eggPosX] != L' ');
 
-			for(int i = 0; i < numEggs; i++)
+			for (int i = 0; i < numEggs; i++)
 				snake.push_back({ snake.at(snake.size() - 1).at(0), snake.at(snake.size() - 1).at(1) });
 		}
 
@@ -153,16 +131,11 @@ int Snake()
 			for (int i = 1; i < snake.size(); i++)
 				screen[snake.at(i).at(1) * screenWidth + snake.at(i).at(0)] = L"x"[0];
 
-			if (score > stoi(num))
-				wsprintf(&screen[10 * screenWidth + 33], L"NEW HIGH SCORE");
-			else
-				wsprintf(&screen[10 * screenWidth + 28], L"CURRENT HIGH SCORE: %d", stoi(num));
-
 			wsprintf(&screen[15 * screenWidth + 18], L"PRESS \"ENTER\" TO PLAY AGAIN OR \"ESC\" TO QUIT");
 			wsprintf(&screen[screenWidth + 36], L"GAME OVER");
 
-			bool newGame = (0x8000 & GetAsyncKeyState((unsigned char)("\x0D"[0]))) != 0;
-			bool quit = (0x8000 & GetAsyncKeyState((unsigned char)("\x1B"[0]))) != 0;
+			newGame = (0x8000 & GetAsyncKeyState((unsigned char)("\x0D"[0]))) != 0;
+			quit = (0x8000 & GetAsyncKeyState((unsigned char)("\x1B"[0]))) != 0;
 
 			WriteConsoleOutputCharacter(hConsole, screen, screenArea, { 0, 0 }, &dwBytesWritten);
 
